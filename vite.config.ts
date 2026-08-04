0c273cc9c7cccab2f40913ad9dea7e9739af6863
Geoffrey Millar <gmillar6@gmail.com>
2026-08-04 13:37:14 -0400

    fix: dedupe React in Vite development

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,31 @@ import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	base: './',
-	plugins: [
-		nodePolyfills({
-			include: ['buffer', 'crypto', 'process', 'stream', 'util'],
-			protocolImports: true,
-		}),
-		react(),
-	],
-	resolve: {
-		alias: {
-			api: path.resolve(__dirname, 'src/api'),
-			components: path.resolve(__dirname, 'src/components'),
-			helpers: path.resolve(__dirname, 'src/helpers'),
-			providers: path.resolve(__dirname, 'src/providers'),
-		},
-	},
-	build: {
-		outDir: 'dist',
-		emptyOutDir: true,
-		sourcemap: true,
-	},
-	server: {
-		host: '127.0.0.1',
-		port: 3000,
-		strictPort: true,
-	},
+  base: './',
+  plugins: [
+    nodePolyfills({
+      include: ['buffer', 'crypto', 'process', 'stream', 'util'],
+      protocolImports: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+    alias: {
+      api: path.resolve(__dirname, 'src/api'),
+      components: path.resolve(__dirname, 'src/components'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      providers: path.resolve(__dirname, 'src/providers'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    strictPort: true,
+  },
 });
