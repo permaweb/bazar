69e1e560f1875e319c27203af0c2b3aebc3246aa
Geoffrey Millar <gmillar6@gmail.com>
2026-08-07 12:25:21 -0400

    perf: externalize names cube animation

--- src/components/NamesCubePreview.tsx
+++ src/components/NamesCubePreview.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import arweaveNamesCube from '../assets/arweave-names-cube.gif';
 import arweaveNamesCubeStill from '../assets/arweave-names-cube.png';
 
+const arweaveNamesCube = 'https://arweave.net/V2oaZC7f9ZtAlEISMyLjLzWaVR5ky-Nl1tWdmxePSC4';
+
 export function NamesCubePreview() {
-	const [hovered, setHovered] = React.useState(false);
-	return (
-		<span
-			className="names-cube-preview"
-			aria-hidden="true"
-			onMouseEnter={() => setHovered(true)}
-			onMouseLeave={() => setHovered(false)}
-		>
-			<img src={hovered ? arweaveNamesCube : arweaveNamesCubeStill} alt="" decoding="async" loading="lazy" />
-		</span>
-	);
+  const [hovered, setHovered] = React.useState(false);
+  return (
+    <span
+      className="names-cube-preview"
+      aria-hidden="true"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img src={hovered ? arweaveNamesCube : arweaveNamesCubeStill} alt="" decoding="async" loading="lazy" />
+    </span>
+  );
 }
